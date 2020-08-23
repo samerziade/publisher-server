537b5eb4d703f0d6a4a8e339e244786ec83b9113
Samer Ziade <21907+samerziade@users.noreply.github.com>
2020-08-23 10:04:12 -0400

    createSubscription and tests

---
+++ src/mysql/createSubscription.ts
@@ -0,0 +1,8 @@
+import { Connection } from 'mysql2/promise'
+
+import { Subscription } from '../SubscribeController'
+
+export const createSubscription = (conn: Connection, { subscriptions_id }: Subscription, message: string): void => {
+  const query = 'INSERT INTO messages (subscriptions_id, message) VALUES (?, ?)'
+  conn.query(query, [subscriptions_id, message])
+}
