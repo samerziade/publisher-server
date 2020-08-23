d8da9581eadd3f34ba95a7fdc18e9aa955c7e5e6
Samer Ziade <21907+samerziade@users.noreply.github.com>
2020-08-23 08:29:07 -0400

    getMessagesForSubscription and tests

---
+++ src/mysql/__tests__/getMessagesForSubscription.spec.ts
@@ -0,0 +1,24 @@
+import { Connection } from 'mysql2'
+
+import { getMessagesForSubscription } from '..'
+import { Subscription } from '../../SubscribeController'
+
+describe('getMessagesForSubscription()', () => {
+  it('should return an empty array', () => {
+    const conn = {
+      query: jest.fn(() => [[]]),
+    }
+    const subscription: Partial<Subscription> = { subscriptions_id: 1 }
+    const result = getMessagesForSubscription((conn as unknown) as Connection, subscription as Subscription)
+    expect(result).toEqual([])
+  })
+
+  it('should return an array of Message', () => {
+    const conn = {
+      query: jest.fn(() => [[{ messages_id: 1, subscriptions_id: 1, message: 'Test 1', created_at: 'Date 1' }]]),
+    }
+    const subscription: Partial<Subscription> = { subscriptions_id: 1 }
+    const result = getMessagesForSubscription((conn as unknown) as Connection, subscription as Subscription)
+    expect(result).toEqual([{ messages_id: 1, subscriptions_id: 1, message: 'Test 1', created_at: 'Date 1' }])
+  })
+})
---
+++ src/mysql/getMessagesForSubscription.ts
@@ -0,0 +1,9 @@
+import { Connection } from 'mysql2'
+import { Subscription } from '../SubscribeController'
+import { Message } from '../catchAllController'
+
+export const getMessagesForSubscription = (conn: Connection, subscription: Subscription): Message[] => {
+  let query = 'SELECT * FROM messages WHERE subscriptions_id = ?'
+  const [messages] = (conn.query(query, [subscription.subscriptions_id]) as unknown) as [Message[]]
+  return messages
+}
