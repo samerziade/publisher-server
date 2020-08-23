c24fbd3e18c7410e07fbcd881cb904acb28cfa74
Samer Ziade <21907+samerziade@users.noreply.github.com>
2020-08-23 08:08:18 -0400

    catchAll route code and tests

---
+++ src/CatchAllController/__tests__/catchAllController.spec.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express'
+
+import { catchAllController, Message } from '..'
+import { Subscription } from '../../SubscribeController'
+
+let getSubscriptionByRouteReturnValue: () => Subscription
+
+jest.mock('../../mysql', () => ({
+  ...(jest.requireActual('../../mysql') as object),
+  getSubscriptionByRoute: jest.fn(() => getSubscriptionByRouteReturnValue()),
+  getMessagesForSubscription: jest.fn((): Message[] => [
+    {
+      messages_id: 1,
+      subscriptions_id: 1,
+      message: 'Test 1',
+      created_at: 'Date 1',
+    },
+  ]),
+}))
+
+describe('catchAllController()', () => {
+  it('should return a 404 resposne if the subscription is not found', async () => {
+    const request: Partial<Request> = { params: { '0': '' } }
+    const response: Partial<Response> = {}
+    response.status = jest.fn().mockReturnValue(response)
+    response.send = jest.fn().mockReturnValue(response)
+
+    getSubscriptionByRouteReturnValue = () => (undefined as unknown) as Subscription
+
+    await catchAllController(request as Request, response as Response)
+    expect(response.status).toHaveBeenCalledWith(404)
+    expect(response.send).toHaveBeenCalledWith({ error: 'Not found' })
+  })
+
+  it('should return an API response with the messages in a topic', async () => {
+    const request: Partial<Request> = { params: { '0': '' } }
+    const response: Partial<Response> = {}
+    response.send = jest.fn().mockReturnValue(response)
+
+    getSubscriptionByRouteReturnValue = () => ({ subscriptions_id: 1, topic: 'Topic 1', url: '' })
+
+    await catchAllController(request as Request, response as Response)
+
+    expect(response.send).toHaveBeenCalledWith([{ date: 'Date 1', message: 'Test 1' }])
+  })
+
+  it('should catch an error and return a 500 server error', () => {
+    expect.assertions(2)
+
+    const request: Partial<Request> = { params: { '0': '' } }
+    const response: Partial<Response> = {}
+    response.status = jest.fn().mockReturnValue(response)
+    response.send = jest.fn().mockReturnValue(response)
+
+    getSubscriptionByRouteReturnValue = () => {
+      throw new Error('Test Error')
+    }
+
+    console.error = jest.fn()
+
+    catchAllController(request as Request, response as Response).then(() => {
+      expect(response.status).toHaveBeenCalledWith(500)
+      expect(response.send).toHaveBeenCalledWith({ error: 'Server error' })
+    })
+  })
+})
---
+++ src/CatchAllController/controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'
+
+import { connection, getSubscriptionByRoute, getMessagesForSubscription } from '../mysql'
+import { mapMessagesToResponse } from './mapMessagesToResponse'
+
+export const catchAllController = async (req: Request, res: Response) => {
+  try {
+    const conn = await connection()
+    const subscription = getSubscriptionByRoute(conn, req.params[0])
+    if (subscription === undefined) {
+      return res.status(404).send({ error: 'Not found' })
+    }
+
+    res.send(mapMessagesToResponse(getMessagesForSubscription(conn, subscription)))
+  } catch (err) {
+    console.error('ERROR', err)
+    return res.status(500).send({ error: 'Server error' })
+  }
+}
