c368c17ac7b202c75bc7e9fff17d6b055b10f9bd
Samer Ziade <21907+samerziade@users.noreply.github.com>
2020-08-23 10:46:34 -0400

    publishController and tests

---
+++ src/publishController/controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express'
+
+import { connection, getSubscriptionByTopic } from '../mysql'
+import { createSubscription } from '../mysql'
+
+export const controller = async (req: Request, res: Response) => {
+  try {
+    const { topic } = req.params
+    const { message } = req.body
+
+    if (message === undefined || message === '') {
+      return res.status(400).send({ error: 'Invalid message supplied' })
+    }
+
+    const conn = await connection()
+    const subscription = getSubscriptionByTopic(conn, topic)
+
+    if (subscription === undefined) {
+      return res.status(404).send({ error: 'Topic not found' })
+    }
+
+    createSubscription(conn, subscription, message)
+    return res.status(201).send({ message: 'Message published' })
+  } catch (err) {
+    console.error('ERROR', err)
+    return res.status(500).send({ error: 'Server error' })
+  }
+}
