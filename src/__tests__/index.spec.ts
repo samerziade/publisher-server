3d0a10908801479c0eaccfe89c5115e2bd36939f
Samer Ziade <21907+samerziade@users.noreply.github.com>
2020-08-23 08:07:35 -0400

    Remove index test

--- src/__tests__/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { hello } from '../'
-
-it('should print hello', () => {
-  console.log = jest.fn()
-  hello()
-  expect(console.log).toHaveBeenCalledWith('hello')
-  jest.resetAllMocks()
-})
