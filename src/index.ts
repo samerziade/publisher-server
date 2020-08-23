1d990fe3e92bd25a3956d1ab7f2d6dfe74364270
Samer Ziade <21907+samerziade@users.noreply.github.com>
2020-08-23 12:12:14 -0400

    Main index file to run the server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-export const hello = () => console.log('hello')
-hello()
+import express from 'express'
+import bodyParser from 'body-parser'
+
+import { subscribeController } from './subscribeController'
+import { publishController } from './publishController'
+import { catchAllController } from './catchAllController'
+import { getHostname, getPort } from './utils'
+
+const app = express()
+app.listen(getPort(), () => console.info(`Server running on http://${getHostname()}:${getPort()}`))
+app.use(bodyParser.json())
+app.post('/subscribe/:topic', subscribeController)
+app.post('/publish/:topic', publishController)
+app.get('*', catchAllController)
